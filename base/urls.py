cb2b52781fd3daa6462ecb4c8f4c9645c97c9627
Adnan-edu <md.ashikadnan@gmail.com>
2021-04-24 01:33:58 +0600

    added json web token

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,14 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product'),
 ]
