b41013d47777b46922f3513e000e4fc31dbeb9c0
Adnan-edu <md.ashikadnan@gmail.com>
2021-04-24 13:15:04 +0600

    cleaned urls and views

--- base/views/user_views.py
+++ base/views/user_views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+
 
 from django.contrib.auth.models import User
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from .products import products
 
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithTokens
-
-from .models import Product
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithTokens
 
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -66,15 +63,3 @@ def getUsers(request):
     return Response(serializers.data)
 
 
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializers = ProductSerializer(products, many=True)
-    return Response(serializers.data)
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
