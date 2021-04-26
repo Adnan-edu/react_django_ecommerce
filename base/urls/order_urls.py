9090549cfe510df3d7f26b5b62ff70d9c028766d
Adnan-edu <md.ashikadnan@gmail.com>
2021-04-26 17:24:20 +0600

    minor changes in models and order views

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -4,4 +4,5 @@ from base.views import order_views
 
 urlpatterns = [
     path('add/', order_views.addOrderItems, name='orders-add'),
+    path('<str:pk>/', order_views.getOrderById, name='user-order'),
 ]
