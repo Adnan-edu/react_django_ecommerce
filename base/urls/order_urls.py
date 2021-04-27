f57886a3bb4764ac8884833819928dbcd7c00821
Adnan-edu <md.ashikadnan@gmail.com>
2021-04-27 21:30:48 +0600

    created views for order info

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -4,5 +4,7 @@ from base.views import order_views
 
 urlpatterns = [
     path('add/', order_views.addOrderItems, name='orders-add'),
+    path('myorders/', order_views.getMyOrders, name='myorders'),
     path('<str:pk>/', order_views.getOrderById, name='user-order'),
+
 ]
