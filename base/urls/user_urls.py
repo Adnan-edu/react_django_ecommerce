59f9acf973047e9bcde0b88750b28667c664c50d
Adnan-edu <md.ashikadnan@gmail.com>
2021-04-28 19:42:02 +0600

    added views to view user info as an admin

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('profile/', user_views.getUserProfile, name='user-profile'),
     path('profile/update/', user_views.updateUserProfile, name='user-profile-update'),
     path('', user_views.getUsers, name='users'),
+    path('<str:pk>/', user_views.getUserById, name='user'),
+    path('update/<str:pk>/', user_views.updateUser, name='user-update'),
     path('delete/<str:pk>/', user_views.deleteUser, name='user-delete'),
+
+   
 ]
